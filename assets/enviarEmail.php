28a5de4e7603c9a28214a0d70829bc744ccfb16e
Manoel Neto <manoel.neto.morais@gmail.com>
2020-06-29 16:56:35 -0300

    update

---
+++ assets/enviarEmail.php
@@ -0,0 +1,14 @@
+<?php
+
+$assunto = $_POST['assunto'];   
+$corpo = "
+ Nome:      ".$_POST['nome']."
+ Telefone:  ".$_POST['tel']."
+ Assunto:   ".$_POST['assunto']."
+ E-mail:    ".$_POST['email']."
+ Mensagem:  ".$_POST['menssagem']."
+";
+
+mail("manoel.neto.morais@gmail.com", $assunto, $corpo, "From: ".$_POST['tel']);
+echo "Dados enviados com sucesso!";
+ 
